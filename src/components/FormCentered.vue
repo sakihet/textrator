cef4c9e1cb5874df54622516fa197e54eed1dfaf
saki <sakihet@gmail.com>
2020-01-19 12:52:45 +0900

    add FormCentered component

---
+++ src/components/FormCentered.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-centered">
+    <div class="cell left">
+      <slot name="leftContent"></slot>
+    </div>
+    <div class="cell right">
+      <slot name="rightContent"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormCentered',
+}
+</script>
+
+<style scoped>
+.form-centered {
+  display: flex;
+}
+.cell {
+  flex: 1;
+}
+.left {
+  text-align: right;
+}
+.right {
+  text-align: left;
+}
+</style>
