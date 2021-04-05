f41a8ee5916cd783da16c0031ee5d82687286938
saki <sakihet@gmail.com>
2021-04-05 23:18:36 +0900

    add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AppHeader
+      :headerText="name"
+      :headerDescription="description"
+    />
+    <p>version: {{ version }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { name, description, version } from '../package.json'
+import AppHeader from './components/AppHeader'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
+  },
+  data () {
+    return {
+      name: name[0].toUpperCase() + name.slice(1),
+      description: description,
+      version: version
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ headerText }}</h1>
+    <p>{{ headerDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+    headerText: {
+      type: String,
+      required: true
+    },
+    headerDescription: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
