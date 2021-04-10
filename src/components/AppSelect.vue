b0463947aa2ac3418dd37dc0a4b257c2b3758dd5
saki <sakihet@gmail.com>
2021-04-10 10:44:34 +0900

    add AppSelect, SelectLabeled

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <select @change="$emit('select', $event.target.value)">
+      <option selected>-</option>
+      <option
+        v-for="optionItem in optionList"
+        :key="optionItem.id"
+        :value="optionItem.id"
+      >{{ optionItem.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSelect',
+  props: {
+    optionList: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/SelectLabeled.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="select-labeled">
+    <div class="left">
+      <label :for="selectId">
+        {{ labelName }}
+      </label>
+    </div>
+    <div class="right">
+      <AppSelect
+        :id="selectId"
+        :optionList = "optionList"
+        @select="$emit('select', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppSelect from './AppSelect'
+
+export default {
+  name: 'SelectLabeled',
+  components: {
+    AppSelect
+  },
+  props: {
+    selectId: {
+      type: String,
+      required: true
+    },
+    labelName: {
+      type: String
+    },
+    value: [String, Number, Boolean],
+    optionList: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+.select-labeled {
+  display: flex;
+}
+.left {
+  flex: 1;
+  text-align: right;
+  padding-right: 5px;
+}
+.right {
+  flex: 1;
+  text-align: left;
+}
+</style>
