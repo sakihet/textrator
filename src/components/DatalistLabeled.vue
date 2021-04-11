366a5f22303f440784f658048e1114279d5b287f
saki <sakihet@gmail.com>
2021-04-11 17:16:25 +0900

    add DatalistLabeled

---
+++ src/components/DatalistLabeled.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="datalist-labeled">
+    <div class="left">
+      <label :for="selectId">
+        {{ labelName }}
+      </label>
+    </div>
+    <div class="right">
+      <AppDatalist
+        :id="selectId"
+        :datalist = "datalistList"
+        @select="$emit('select', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppDatalist from './AppDatalist'
+
+export default {
+  name: 'DatalistLabeled',
+  components: {
+    AppDatalist
+  },
+  props: {
+    selectId: {
+      type: String,
+      required: true
+    },
+    labelName: {
+      type: String
+    },
+    value: [String, Number, Boolean],
+    datalistList: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+.datalist-labeled {
+  display: flex;
+}
+.left {
+  flex: 1;
+  text-align: right;
+  padding-right: 5px;
+}
+.right {
+  flex: 1;
+  text-align: left;
+}
+</style>
