79ea31e80fe46bed0b4fd063f45355e38befb689
saki <sakihet@gmail.com>
2021-04-20 22:32:51 +0900

    add AppInputCheckbox

---
+++ src/components/AppInputCheckbox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+      :id="inputId"
+      type="checkbox"
+      :value="value"
+      @change="$emit('change', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInputCheckbox',
+  props: {
+    inputId: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Boolean
+    }
+  },
+  emits: [
+    'change'
+  ]
+}
+</script>
+
+<style scoped>
+</style>
