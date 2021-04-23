abe932a62dda59ab57731251225635e6bbc5549b
saki <sakihet@gmail.com>
2021-04-23 23:57:53 +0900

    add AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button>
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    text: String,
+  }
+}
+</script>
+
+<style scoped>
+</style>
